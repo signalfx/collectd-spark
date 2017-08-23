14f5195d1c338aca0ce0c497e9d9c8694c1d9aaa
Samuel Lu <samuel@signalfx.com>
2017-08-23 10:48:31 -0700

    updated verify.sh script to fail if command is not successful

--- verify.sh
+++ verify.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 flake8 spark_plugin.py test_spark_plugin.py test_metric_sink.py test_spark_plugin_manager.py test_spark_agent.py test_html.py sample_responses.py
 if [ "$?" -ne 0 ]; then
     exit 1;
